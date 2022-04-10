7526b908ada67e4c4e5cd85e7b2ec2866bb88e7a
Hercute <hercute.academy@gmail.com>
2022-04-09 17:53:04 -0700

    Hello puts, task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
 
  /**
   * main - entry point
-  *
-  * Return: 0 if success. 
+  * Return: 0 if success.
   */
-int main(void) 
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"); 
- return (0); 
+puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
